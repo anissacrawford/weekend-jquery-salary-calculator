c1b5297605341cd0a7227f119e816a308050701b
Anissa Crawford <anissacrawford@gmail.com>
2022-03-04 16:01:23 -0600

    table adds inputs

--- client.js
+++ client.js
@@ -1,12 +1,52 @@
 // console.log('hi!')
 
+let employees = [];
+
 $(ready);
 
 function ready(){
+
     // console.log('hi')
-    $('#submitBtn').on('click', handleSubmit);
+    $('#submitBtn').on('click', addEmployee);
+
 };
 
-function handleSubmit (){
-    console.log('hi');
+function addEmployee (){
+
+    //creates new employee with inputs 
+    let newEmployee = {
+        firstName: $(firstName).val(),
+        lastName: $(lastName).val(),
+        id: $(id).val(),
+        title: $(title).val(),
+        salary: $(salary).val(),
+    }
+
+    //pushes to employees array 
+    employees.push(newEmployee);
+    console.log(employees);
+
+    // clears values
+    $(firstName).val('');
+    $(lastName).val('');
+    $(id).val('');
+    $(title).val('');
+    $(salary).val('');
+
+    addToDom();
+}
+
+
+function addToDom(){
+for (let employee of employees){
+    $('#tableBody').append(
+        `<tr>
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.id}</td>
+        <td>${employee.title}</td>
+        <td>${employee.salary}</td>
+        </tr>`
+    )
+    }
 }
